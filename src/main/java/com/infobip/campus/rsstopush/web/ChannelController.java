84647bd5a58746b091225e81dbc6c8a4aac65fcd
Goran Obarcanin <gobarcanin@gmail.com>
2013-11-09 16:10:39 +0100

    ChannelController v2 refactored

--- src/main/java/com/infobip/campus/rsstopush/web/ChannelController.java
+++ src/main/java/com/infobip/campus/rsstopush/web/ChannelController.java
@@ -4,48 +4,53 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.infobip.campus.rsstopush.channels.ChannelHandler;
 import com.infobip.campus.rsstopush.channels.ChannelModel;
+import com.infobip.campus.rsstopush.channels.DefaultChannelService;
 
 @RequestMapping("/channel/**")
 @Controller
 public class ChannelController {
 
+	@Autowired
+	DefaultChannelService defaultChannelService;
+
 	@RequestMapping(method = RequestMethod.GET, value = "/fetch")
 	@ResponseBody
 	public List<ArrayList<ChannelModel>> fetchChannelList() {
-		/* zasada null */
-		ChannelHandler handler = new ChannelHandler();
-		return Arrays.asList(handler.fetchChannelList());
+
+		return Arrays.asList(defaultChannelService.fetchChannelList());
+
 	}
 
 	@RequestMapping(value = "/add", method = RequestMethod.POST, consumes = "application/json")
 	@ResponseBody
 	public String addChannel(@RequestBody ChannelModel model) {
-		ChannelHandler handler = new ChannelHandler();
-		if (handler.addChannel(model) == true) {
+
+		if (defaultChannelService.addChannel(model) == true) {
 			return "success";
 		} else {
 			return "fail";
 		}
+
 	}
 
 	@RequestMapping(value = "/delete", method = RequestMethod.POST)
 	@ResponseBody
 	public String deleteChannel(@RequestBody String value) {
 		ChannelModel model = new ChannelModel(value, "");
-		ChannelHandler handler = new ChannelHandler();
 
-		if (handler.deleteChannel(model) == true) {
+		if (defaultChannelService.deleteChannel(model) == true) {
 			return "success";
 		} else {
 			return "false";
 		}
+
 	}
 }
