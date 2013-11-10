1d7d3490d1de28be9f958229dceb41845418de01
Nadin Zajimovic <zayim>
2013-11-10 14:13:41 +0100

    Dodavanje equals i hashCode za RssFeedModel

--- src/main/java/com/infobip/campus/rsstopush/models/RssFeedModel.java
+++ src/main/java/com/infobip/campus/rsstopush/models/RssFeedModel.java
@@ -1,4 +1,5 @@
 package com.infobip.campus.rsstopush.models;
+
 import org.springframework.roo.addon.javabean.RooJavaBean;
 import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
 import org.springframework.roo.addon.tostring.RooToString;
@@ -7,13 +8,32 @@ import org.springframework.roo.addon.tostring.RooToString;
 @RooToString
 @RooJpaActiveRecord
 public class RssFeedModel {
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((rssUrl == null) ? 0 : rssUrl.hashCode());
+		return result;
+	}
 
-    /**
-     */
-    private String rssUrl;
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		RssFeedModel other = (RssFeedModel) obj;
+		if (rssUrl == null) {
+			if (other.rssUrl != null)
+				return false;
+		} else if (!rssUrl.equals(other.rssUrl))
+			return false;
+		return true;
+	}
 
-    /**
-     */
-    private String description;
+	private String rssUrl;
+	private String description;
 
 }
