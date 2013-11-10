d0e4a656cfcc94bfe0cdcd26b1a1d6ec1307c7ff
Nadin Zajimovic <zayim>
2013-11-10 15:33:18 +0100

    Ne znam sta sam radio

--- src/main/java/com/infobip/campus/rsstopush/models/RssFeedModel.java
+++ src/main/java/com/infobip/campus/rsstopush/models/RssFeedModel.java
@@ -1,5 +1,6 @@
 package com.infobip.campus.rsstopush.models;
 
+import org.springframework.roo.addon.equals.RooEquals;
 import org.springframework.roo.addon.javabean.RooJavaBean;
 import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
 import org.springframework.roo.addon.tostring.RooToString;
