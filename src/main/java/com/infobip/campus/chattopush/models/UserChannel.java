99bc4a25aabd962d51f28b0b368d40289a2b5937
Elmir Sukanovic <elmirsukanovic@gmail.com>
2013-11-11 15:05:28 +0100

    Modeli za tablice

--- src/main/java/com/infobip/campus/chattopush/models/UserChannel.java
+++ src/main/java/com/infobip/campus/chattopush/models/UserChannel.java
@@ -2,7 +2,15 @@ package com.infobip.campus.chattopush.models;
 import org.springframework.roo.addon.javabean.RooJavaBean;
 import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
 import org.springframework.roo.addon.tostring.RooToString;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
+import java.util.Date;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @RooJavaBean
 @RooToString
@@ -11,6 +19,21 @@ public class UserChannel {
 
     /**
      */
-    @ManyToOne
-    private ChannelModel UserModel;
+    @ManyToMany(cascade = CascadeType.ALL)
+    private List<UserModel> users = new ArrayList<UserModel>();
+
+    /**
+     */
+    @ManyToMany(cascade = CascadeType.ALL)
+    private List<ChannelModel> channels = new ArrayList<ChannelModel>();
+
+    /**
+     */
+    private String status;
+
+    /**
+     */
+    @Temporal(TemporalType.TIMESTAMP)
+    @DateTimeFormat(style = "M-")
+    private Date lastMessageDate;
 }
