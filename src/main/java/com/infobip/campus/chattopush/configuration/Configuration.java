3808780ed61a93843fb3b8c4972f8e134e277a07
Elmir Sukanovic <elmirsukanovic@gmail.com>
2013-11-11 17:56:52 +0100

    Dodana klasa Configuration

--- src/main/java/com/infobip/campus/chattopush/configuration/Configuration.java
+++ src/main/java/com/infobip/campus/chattopush/configuration/Configuration.java
@@ -3,8 +3,7 @@ package com.infobip.campus.chattopush.configuration;
 import java.util.Date;
 
 public class Configuration {
-	public static final String APPLICATION_ID = "9cabf301d3db";
-	public static final Date DEFAULT_DATE = new Date(92, 2, 6);
+	public static final String APPLICATION_ID = "1a6cfc8b976c";
 	public static final String AUTHORIZATION_INFO = "Basic cHVzaGRlbW86cHVzaGRlbW8=";
 
 }
