7aa3582450af4f6e74b37b59869be21c07be40d4
esukanovic <elmirsukanovic@gmail.com>
2013-11-12 12:53:40 +0100

    Implemetirana metoda fetchMessageList unutar MessageControllera

--- src/main/java/com/infobip/campus/chattopush/controllers/ChannelController.java
+++ src/main/java/com/infobip/campus/chattopush/controllers/ChannelController.java
@@ -1,6 +1,5 @@
 package com.infobip.campus.chattopush.controllers;
 
-import java.util.Arrays;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
--- src/main/java/com/infobip/campus/chattopush/services/UserService.java
+++ src/main/java/com/infobip/campus/chattopush/services/UserService.java
@@ -1,7 +1,5 @@
 package com.infobip.campus.chattopush.services;
 
-import com.infobip.campus.chattopush.models.ChannelModel;
-
 public interface UserService {
 	
 	public abstract boolean  loginUser();
