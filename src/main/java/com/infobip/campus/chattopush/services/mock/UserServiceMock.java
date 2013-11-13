c8df5fc65a6807749859377abbe8337156a47af9
Goran Obarcanin <gobarcanin@gmail.com>
2013-11-13 12:46:25 +0100

    projekt prebachen na mock

---
+++ src/main/java/com/infobip/campus/chattopush/services/mock/UserServiceMock.java
@@ -0,0 +1,47 @@
+package com.infobip.campus.chattopush.services.mock;
+
+import java.util.List;
+
+import com.infobip.campus.chattopush.models.UserModel;
+import com.infobip.campus.chattopush.models.UsersChannels;
+import com.infobip.campus.chattopush.services.UserService;
+
+public class UserServiceMock implements UserService {
+
+	@Override
+	public String loginUser(UserModel _model) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String registerUser(UserModel _model) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String deleteUser(UserModel _model) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String checkUserExists(UserModel _model) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<UserModel> fetchAllUsers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String addChannelToUser(UsersChannels _model) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
