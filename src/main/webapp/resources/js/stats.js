8397fea0fb5cd2b852a4070bf96cae3f721fc659
Imer Husejnovic <imer90@gmail.com>
2013-11-14 14:57:53 +0100

    Napravljen graficki prikaz broja poruka po danima

--- src/main/webapp/resources/js/stats.js
+++ src/main/webapp/resources/js/stats.js
@@ -1,84 +1,86 @@
-function statsByDays(){
-	
-	var byDays;
-	var statsSent;
-	var statsReceived;
-	$(function () {
-		$.ajax({
-			url: "https://pushapi.infobip.com/1/statistics/application/9cabf301d3db/notifications?time-type=month&month=11&year=2013",
-			headers: {
-				'Accept': 'text/plain',
-				'Content-type':'application/json',
-				'Authorization': 'Basic cHVzaGRlbW86cHVzaGRlbW8=',
-			},
-			method: 'GET',
-			contentType: 'application/json',
-			success:function(data, status,xhr){
-				byDays=data.data[0];
-				alert(JSON.stringify(byDays));
-				statsSent='['+byDays.countSent+']';
-				alert(statsSent);
-				statsReceived='['+byDays.countReceived+']';
-				alert(statsReceived);
-				}
-		});
-	
+function graph(statsSent, statsReceived) {
+	$('#container').highcharts({
+		chart : {
+			type : 'area'
+		},
+		title : {
+			text : 'Statistics: day/messages'
+		},
+		subtitle : {
+			text : 'Source: <a href="http://infobip.com">' + 'infobip</a>'
+		},
+		xAxis : {
 
-		$(function () {
-	        $('#container').highcharts({
-	            chart: {
-	                type: 'area'
-	            },
-	            title: {
-	                text: 'Statistics'
-	            },
-	            subtitle: {
-	                text: 'Source: <a href="http://infobip.com">'+
-	                    'infobip</a>'
-	            },
-	            xAxis: {
-	                labels: {
-	                    formatter: function() {
-	                        return this.value; // clean, unformatted number for year
-	                    }
-	                }
-	            },
-	            yAxis: {
-	                title: {
-	                    text: 'Messages'
-	                },
-	                labels: {
-	                    formatter: function() {
-	                        return this.value;
-	                    }
-	                }
-	            },
-	            tooltip: {
-	                pointFormat: '{series.name} <b>{point.y:,.0f}</b>'
-	            },
-	            plotOptions: {
-	                area: {
-	                    pointStart: 1,
-	                    marker: {
-	                        enabled: false,
-	                        symbol: 'circle',
-	                        radius: 1,
-	                        states: {
-	                            hover: {
-	                                enabled: true
-	                            }
-	                        }
-	                    }
-	                }
-	            },
-	            series: [{
-	                name: 'Sent',
-	                data: [100,200,300]
-	            }, {
-	                name: 'Received',
-	                data: [200,500,600]
-	            }]
-	        });
-	    });
-    });
+		},
+		yAxis : {
+			title : {
+				text : 'Messages'
+			},
+			labels : {
+				formatter : function() {
+					return this.value;
+				}
+			}
+		},
+		tooltip : {
+			pointFormat : '{series.name} <b>{point.y:,.0f}</b>'
+		},
+		plotOptions : {
+			area : {
+				pointStart : 1,
+				marker : {
+					enabled : false,
+					symbol : 'circle',
+					radius : 1,
+					states : {
+						hover : {
+							enabled : true
+						}
+					}
+				}
+			}
+		},
+		series : [ {
+			name : 'Sent',
+			data : statsSent
+		}, {
+			name : 'Received',
+			data : statsReceived
+		} ]
+	});
+
+}
+
+function statsByDays() {
+
+
+	var statsSent = null;
+	var statsReceived = null;
+
+	$
+			.ajax({
+				url : "https://pushapi.infobip.com/1/statistics/application/1a6cfc8b976c/notifications?time-type=month&month=11&year=2013",
+				headers : {
+					'Accept' : 'text/plain',
+					'Content-type' : 'application/json',
+					'Authorization' : 'Basic cHVzaGRlbW86cHVzaGRlbW8=',
+				},
+				method : 'GET',
+				contentType : 'application/json',
+				success : function(data, status, xhr) {
+
+					statsSent = data.data[0].countSent;
+					statsReceived=data.data[0].countReceived;
+					//alert(byDays2);
+					//statsSent=JSON.stringify(byDays);
+					//statsReceived=JSON.stringify(byDays2);
+					// statsSent=JSON.stringify(byDays.countSent[0]);
+
+					// alert(statsSent);
+					// statsReceived=byDays.countReceived;
+					// alert(statsReceived);
+					graph(statsSent, statsReceived);
+				}
+
+			});
 }
