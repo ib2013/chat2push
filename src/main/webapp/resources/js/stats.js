242af557b9242b4ccd7db2d03fc35ed1c5a6d1fb
Imer Husejnovic <imer90@gmail.com>
2013-11-16 21:40:08 +0100

    Graf dodati sati

--- src/main/webapp/resources/js/stats.js
+++ src/main/webapp/resources/js/stats.js
@@ -1,4 +1,4 @@
-var appId = "1a6cfc8b976c"; //id aplikacije na infobip
+var appId = "1a6cfc8b976c"; // id aplikacije na infobip
 function allStats() {
 
 	statsByDays();
@@ -6,74 +6,86 @@ function allStats() {
 }
 
 function graphDays(statsSent, statsReceived, month, year) {
-	var date=new Date();
+	var date = new Date();
 	var thisMonth = date.getMonth();
 	var thisYear = date.getFullYear();
 
-        $('#stats').highcharts({
-            chart: {
-                zoomType: 'x',
-                spacingRight: 20
-            },
-            title: {
-                text: 'Stats days'
-            },
-            subtitle: {
-                text: document.ontouchstart === undefined ?
-                    'Click and drag in the plot area to zoom in' :
-                    'Pinch the chart to zoom in'
-            },
-            xAxis: {
-                type: 'datetime',
-                maxZoom: 14 * 24 * 3600000, // fourteen days
-                title: {
-                    text: null
-                }
-            },
-            yAxis: {
-                title: {
-                    text: 'Messages'
-                }
-            },
-            tooltip: {
-                shared: true
-            },
-            legend: {
-                enabled: false
-            },
-            plotOptions: {
-                area: {
-                    fillColor: {
-                        linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1},
-                        stops: [
-                            [0, Highcharts.getOptions().colors[0]],
-                            [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
-                        ]
-                    },
-                    lineWidth: 1,
-                    marker: {
-                        enabled: false
-                    },
-                    shadow: false,
-                    states: {
-                        hover: {
-                            lineWidth: 1
-                        }
-                    },
-                    threshold: null
-                }
-            },
-    
-            series: [{
-                type: 'area',
-                name: 'Messages',
-                pointInterval: 24 * 3600 * 1000,
-                pointStart: Date.UTC(thisYear, thisMonth, 01),
-                data: statsSent
-            }]
-        });
-   
-    
+	$('#stats')
+			.highcharts(
+					{
+						chart : {
+							zoomType : 'x',
+							spacingRight : 20
+						},
+						title : {
+							text : 'Stats days'
+						},
+						subtitle : {
+							text : document.ontouchstart === undefined ? 'Click and drag in the plot area to zoom in'
+									: 'Pinch the chart to zoom in'
+						},
+						xAxis : {
+							type : 'datetime',
+							maxZoom : 14 * 24 * 3600000, // fourteen days
+							title : {
+								text : null
+							}
+						},
+						yAxis : {
+							title : {
+								text : 'Messages'
+							}
+						},
+						tooltip : {
+							shared : true
+						},
+						legend : {
+							enabled : false
+						},
+						plotOptions : {
+							area : {
+								fillColor : {
+									linearGradient : {
+										x1 : 0,
+										y1 : 0,
+										x2 : 0,
+										y2 : 1
+									},
+									stops : [
+											[
+													0,
+													Highcharts.getOptions().colors[0] ],
+											[
+													1,
+													Highcharts
+															.Color(
+																	Highcharts
+																			.getOptions().colors[0])
+															.setOpacity(0).get(
+																	'rgba') ] ]
+								},
+								lineWidth : 1,
+								marker : {
+									enabled : false
+								},
+								shadow : false,
+								states : {
+									hover : {
+										lineWidth : 1
+									}
+								},
+								threshold : null
+							}
+						},
+
+						series : [ {
+							type : 'area',
+							name : 'Messages',
+							pointInterval : 24 * 3600 * 1000,
+							pointStart : Date.UTC(thisYear, thisMonth, 01),
+							data : statsSent
+						} ]
+					});
 
 }
 
@@ -179,57 +191,88 @@ function graphRooms(data) {
 }
 
 function graphToday(statsSent, statsReceived) {
-	$('#stats2').highcharts({
-		chart : {
-			type : 'area'
-		},
-		title : {
-			text : 'Stats today: messages/hour'
-		},
-		subtitle : {
-			text : 'Source: <a href="http://infobip.com">' + 'infobip</a>'
-		},
-		xAxis : {
-			title : {
-				text : 'Hours'
-			},
-		},
-		yAxis : {
-			title : {
-				text : 'Messages'
-			},
-			labels : {
-				formatter : function() {
-					return this.value;
-				}
-			}
-		},
-		tooltip : {
-			pointFormat : '{series.name} <b>{point.y:,.0f}</b>'
-		},
-		plotOptions : {
-			area : {
-				pointStart : 1,
-				marker : {
-					enabled : false,
-					symbol : 'circle',
-					radius : 1,
-					states : {
-						hover : {
-							enabled : true
-						}
-					}
-				}
-			}
-		},
-		series : [ {
-			name : 'Sent',
-			data : statsSent
-		}, {
-			name : 'Received',
-			data : statsReceived
-		} ]
-	});
+
+	var date = new Date();
+	var thisDay = date.getDate();
+	var thisMonth = date.getMonth();
+	var thisYear = date.getFullYear();
+
+	$('#stats2')
+			.highcharts(
+					{
+						chart : {
+							zoomType : 'x',
+							spacingRight : 20
+						},
+						title : {
+							text : 'Stats hours'
+						},
+						subtitle : {
+							text : document.ontouchstart === undefined ? 'Click and drag in the plot area to zoom in'
+									: 'Pinch the chart to zoom in'
+						},
+						xAxis : {
+							type : 'datetime',
+							maxZoom : 3600000, // fourteen days
+							title : {
+								text : null
+							}
+						},
+						yAxis : {
+							title : {
+								text : 'Messages'
+							}
+						},
+						tooltip : {
+							shared : true
+						},
+						legend : {
+							enabled : false
+						},
+						plotOptions : {
+							area : {
+								fillColor : {
+									linearGradient : {
+										x1 : 0,
+										y1 : 0,
+										x2 : 0,
+										y2 : 1
+									},
+									stops : [
+											[
+													0,
+													Highcharts.getOptions().colors[0] ],
+											[
+													1,
+													Highcharts
+															.Color(
+																	Highcharts
+																			.getOptions().colors[0])
+															.setOpacity(0).get(
+																	'rgba') ] ]
+								},
+								lineWidth : 1,
+								marker : {
+									enabled : false
+								},
+								shadow : false,
+								states : {
+									hover : {
+										lineWidth : 1
+									}
+								},
+								threshold : null
+							}
+						},
+
+						series : [ {
+							type : 'area',
+							name : 'Messages',
+							pointInterval : 3600 * 1000,
+							pointStart : Date.UTC(thisYear, thisMonth, thisDay),
+							data : statsSent
+						} ]
+					});
 
 }
 
@@ -243,8 +286,8 @@ function statsByDays() {
 
 	$.ajax({
 		url : "https://pushapi.infobip.com/1/statistics/application/" + appId
-				+ "/notifications?time-type=month&month="
-				+ thisMonth + "&year=" + thisYear + "",
+				+ "/notifications?time-type=month&month=" + thisMonth
+				+ "&year=" + thisYear + "",
 		headers : {
 			'Accept' : 'text/plain',
 			'Content-type' : 'application/json',
@@ -274,8 +317,8 @@ function statsToday() {
 
 	$.ajax({
 		url : "https://pushapi.infobip.com/1/statistics/application/" + appId
-				+ "/notifications?time-type=day&day=" + thisDay
-				+ "&month=" + thisMonth + "&year=" + thisYear + "",
+				+ "/notifications?time-type=day&day=" + thisDay + "&month="
+				+ thisMonth + "&year=" + thisYear + "",
 		headers : {
 			'Accept' : 'text/plain',
 			'Content-type' : 'application/json',
