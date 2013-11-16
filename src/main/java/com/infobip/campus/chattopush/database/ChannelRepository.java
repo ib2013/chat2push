464b0aedbcb3507b7b2a3a6914c63897466ce5e4
mradosavljevic <m.radosavljevic@gmail.com>
2013-11-16 15:44:56 +0100

    popravljeni upiti

--- src/main/java/com/infobip/campus/chattopush/database/ChannelRepository.java
+++ src/main/java/com/infobip/campus/chattopush/database/ChannelRepository.java
@@ -25,10 +25,7 @@ public class ChannelRepository {
 		Query queryDeleteRelations = em
 				.createQuery("DELETE FROM UsersChannels uc WHERE uc.channel=:cn ");
 		queryDeleteRelations.setParameter("cn", channelName);
-		int n = queryDeleteRelations.executeUpdate();
-		System.out.println(n);
-		
-
+		queryDeleteRelations.executeUpdate();
 	}
 
 	public void removeUserFromRoomDb(UsersChannels relation) {
@@ -71,21 +68,20 @@ public class ChannelRepository {
 
 		return result;
 	}
-/*
-	public UserActivityModel selectAllUsersInChannelDb(ChannelModel channel) {
-		Query query = em
-				.createQuery("SELECT cm FROM UsersChannels cm WHERE cm.channel=:cn ");
-		query.setParameter("cn", channel.getName());
-		@SuppressWarnings("unchecked")
-		List<UserActivityModel> cm = query.getResultList();
-
-		UserActivityModel result = null;
-
-		if (cm.size() > 0) {
-			result = cm.get(0);
-		}
-
-		return result;
-	}*/
+	/*
+	 * public UserActivityModel selectAllUsersInChannelDb(ChannelModel channel)
+	 * { Query query = em
+	 * .createQuery("SELECT cm FROM UsersChannels cm WHERE cm.channel=:cn ");
+	 * query.setParameter("cn", channel.getName());
+	 * 
+	 * @SuppressWarnings("unchecked") List<UserActivityModel> cm =
+	 * query.getResultList();
+	 * 
+	 * UserActivityModel result = null;
+	 * 
+	 * if (cm.size() > 0) { result = cm.get(0); }
+	 * 
+	 * return result; }
+	 */
 
 }
